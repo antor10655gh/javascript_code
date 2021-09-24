5ff42785d238e2da763e305d2f13e19ddf2e173a
AM Antor <asarvinantor1111@gmail.com>
2021-09-24 20:58:44 +0600

    add temperature.js file

---
+++ temperature.js
@@ -0,0 +1,12 @@
+// function alert_sms(){
+// 	alert("Are You Ready");
+// }
+
+function calcuate(){
+	var f = prompt("Enter a Farhenhite value: ");
+
+	var celsius = (f - 32) * (5/9);
+	return celsius;
+}
+
+document.write("Celcius: "+calcuate());
