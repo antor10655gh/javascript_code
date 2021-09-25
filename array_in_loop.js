523d2353a82616ddcfc04145f0f8da916ad59cfa
AM Antor <asarvinantor1111@gmail.com>
2021-09-25 14:11:14 +0600

    add a sum on array_in_loop.js

--- array_in_loop.js
+++ array_in_loop.js
@@ -1,8 +1,11 @@
 let value = [];
+var sum = 0;
 
 for(var i = 0; i<5; i++)
 {
 	value[i] = Number(prompt("Enter a value: "));
+    sum = sum + i;
 }
 
-document.write("All Values: "+ value);
+document.write("All Values: "+ value+"</br>");
+document.write("Sum is: "+ sum);
