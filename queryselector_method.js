b18d933314ba709bb50c65648071efebaa4c74ac
AM Antor <asarvinantor1111@gmail.com>
2021-09-27 01:16:44 +0600

    add querySelector.js

---
+++ queryselector_method.js
@@ -0,0 +1,6 @@
+//Using querySelector()
+document.querySelector("a").innerHTML     = "Click Me";
+document.querySelector("li a").innerHTML  = "Next";
+document.querySelector(".link").innerHTML = "Previous";
+document.querySelector("div a").innerHTML = "Click Here";
+document.querySelector("#link a").innerHTML = "Click Later";
