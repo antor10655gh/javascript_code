42e247fb6bf6bf9a010795050c44930fc30ac112
AM Antor <asarvinantor1111@gmail.com>
2021-09-26 21:59:39 +0600

    add date_methods.js

---
+++ date_methods.js
@@ -0,0 +1,27 @@
+var date = new Date();
+
+document.write(date);
+
+//getDate() method
+var currentdate = date.getDate();
+document.write("</br>"+currentdate);
+
+//getDay() method
+var currentday = date.getDay();
+document.write("</br>"+currentday);
+
+//getMonth() method
+var currentmonth = date.getMonth();
+document.write("</br>"+currentmonth);
+
+//getFullYear() method
+var currentyear = date.getFullYear();
+document.write("</br>"+currentyear);
+
+//getHours() method
+var currenthour = date.getHours();
+document.write("</br>"+currenthour);
+
+//getMinutes() method
+var currentminiuets = date.getMinutes();
+document.write("</br>"+currentminiuets);
