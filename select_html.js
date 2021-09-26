6129fb9c71d88a1120e622cf159b743fac779476
AM Antor <asarvinantor1111@gmail.com>
2021-09-26 23:53:46 +0600

    add select method--getElementsByTagName(),getElementsByClassName(),getElementById() on select_html.js

---
+++ select_html.js
@@ -0,0 +1,11 @@
+//select html code getElementsByTagName()
+document.getElementsByTagName("h1")[0].innerHTML = "How are you ?";
+document.getElementsByTagName("p")[0].innerHTML = "I'm Fine";
+
+//select html code getElementId()
+document.getElementById("heading").innerHTML = "Where are you come from ?";
+document.getElementById("paragraph").innerHTML = "I'm from Jamalpur";
+
+//select html code getElementsByClassName()
+document.getElementsByClassName("heading1")[0].innerHTML = "What is your name ?";
+document.getElementsByClassName("paragraph1")[0].innerHTML = "My name is Antor";
