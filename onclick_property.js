591ead56acb03ebc747ec4e462d505f1273971a8
AM Antor <asarvinantor1111@gmail.com>
2021-09-27 11:43:22 +0600

    add onclick_property.js

---
+++ onclick_property.js
@@ -0,0 +1,11 @@
+var myVar = document.querySelector("#paraId");
+var myPicture = document.querySelector("#pic");
+function myMessage1(){
+	myVar.innerHTML = "Hi, I'm Antor";
+	myPicture.src = "pic1.jpg";
+}
+
+function myMessage2(){
+	myVar.innerHTML = "Hello, I'm Sumaiya";
+	myPicture.src = "pic2.jpg";
+}
