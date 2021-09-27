c82e02393c1a545e98e4d31963c17c16e33aed65
AM Antor <asarvinantor1111@gmail.com>
2021-09-27 23:47:54 +0600

    add a image_slider.js

---
+++ image_slider.js
@@ -0,0 +1,30 @@
+//create slider by using javascript function
+let photos = ["pic1.jpg" , "pic2.jpg" , "pic3.jpg" , "pic4.jpg"];
+var slide_image = document.querySelector("img");
+slide_image.style.height = "300px";
+slide_image.style.width = "400px";
+count = 0;
+
+function next(){
+count++;
+if (count >= photos.length) {
+	count = 0;
+	slide_image.src = photos[count];
+ } 
+
+else{
+	slide_image.src = photos[count];
+ } 
+}
+
+function prev(){
+	count--;
+	if(count<=0){
+		count = photos.length - 1;
+		slide_image.src = photos[count];
+	}
+
+	else{
+		slide_image.src = photos[count];
+	}
+}
