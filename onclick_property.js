3f37255048d4ce57beeba0e5199e8c9528d81c44
AM Antor <asarvinantor1111@gmail.com>
2021-09-27 14:25:51 +0600

    modified oneclick_property.js

--- onclick_property.js
+++ onclick_property.js
@@ -3,9 +3,13 @@ var myPicture = document.querySelector("#pic");
 function myMessage1(){
 	myVar.innerHTML = "Hi, I'm Antor";
 	myPicture.src = "pic1.jpg";
+	myPicture.style.height = "200px";
+	myPicture.style.width = "300px";
 }
 
 function myMessage2(){
 	myVar.innerHTML = "Hello, I'm Sumaiya";
 	myPicture.src = "pic2.jpg";
+	myPicture.style.height = "200px";
+	myPicture.style.width = "300px";
 }
