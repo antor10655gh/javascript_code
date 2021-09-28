bbf93e2c790348f14a26306b8279467ef7a752bc
AM Antor <asarvinantor1111@gmail.com>
2021-09-28 12:42:05 +0600

    add all js file on JS folder

--- JS/image_slider.js
+++ JS/image_slider.js
@@ -3,6 +3,12 @@ let photos = ["pic1.jpg" , "pic2.jpg" , "pic3.jpg" , "pic4.jpg"];
 var slide_image = document.querySelector("img");
 slide_image.style.height = "300px";
 slide_image.style.width = "400px";
+
+var mySlide = document.querySelector(".slider");
+mySlide.style.display = "block";
+mySlide.style.marginLeft = "auto";
+mySlide.style.marginRight = "auto";
+mySlide.style.width = "50%";
 count = 0;
 
 function next(){
