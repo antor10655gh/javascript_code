3d091ba071ef6cf026beb5ed31fd7c0ef8fbaa27
AM Antor <asarvinantor1111@gmail.com>
2021-09-29 02:09:02 +0600

    modifie add_audio.js

--- JS/add_audio.js
+++ JS/add_audio.js
@@ -2,22 +2,21 @@ var len = document.querySelectorAll(".myButton").length;
 for(var i = 0; i < len; i++){
     document.querySelectorAll(".myButton")[i].addEventListener("click", function(){
         var text = this.innerHTML;
-        console.log(text);
 
         switch(text){
             case "a":
                 var audio = new Audio(sound/a.mp3);
-                  audio.play();
+                audio.play();
                 break;
 
             case "b":
                 var audio = new Audio(sound/b.mp3);
-                  audio.play();
+                audio.play();
                 break;
 
             case "c":
                 var audio = new Audio(sound/c.mp3);
-                  audio.play();
+                audio.play();
                 break;
         }
     })
